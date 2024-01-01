760b2e1dc6cd7f36af21b444c78e104e41f42420
titungdup <dup.titung@gmail.com>
2024-01-01 17:09:04 +1030

    chore: update to nextjs 14 and use experimental lightningcss with turbo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    useLightningcss: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
