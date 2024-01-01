395c942a2c7b9a160f8b503d0ed4ee7e4a50e689
titungdup <dup.titung@gmail.com>
2024-01-01 17:09:37 +1030

    style: change homepage ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-7">
-      <div className=" my-10 max-w-xl ">
-        <h2 className="mb-3 text-7xl font-bold">Hi, I&apos;m Dhondup</h2>
-        <p className="mb-4 text-lg font-light">
-          I am a frontend developer based in Australia.
-        </p>
+    <main className="min-h-screen w-full p-7">
+      <div className="mx-auto my-10 flex w-full max-w-screen-lg justify-center gap-10 font-light">
+        <div className="shrink-0">
+          <h2 className="mb-3 text-5xl font-bold">Hi, I&apos;m Dhondup</h2>
+          <p className="mb-2 text-lg ">
+            A frontend developer based in Australia.
+          </p>
+          <p>
+            You can find me on{" "}
+            <a href="http://twitter.com/titungdup" className="underline">
+              Twitter
+            </a>
+          </p>
+        </div>
         <div className="space-y-4 font-light">
           <p>
             In order to up my UI game, I am doing a UI challenge where I build
@@ -20,12 +28,6 @@ export default function Home() {
             I will mostly be following <span className="italic">iCodeThis</span>
             , <span className="italic">Dribbble</span> and{" "}
             <span className="italic">Behance</span> for UI inspiration.
-          </p>
-          <p>
-            You can find me on{" "}
-            <a href="http://twitter.com/titungdup" className="underline">
-              Twitter
-            </a>
           </p>
         </div>
       </div>
