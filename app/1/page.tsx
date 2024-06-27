e88e47be1eb2360d71f977d1e0f684bf345c973d
titungdup <dup.titung@gmail.com>
2024-06-27 13:34:52 +0930

    update: booking ui

--- app/1/page.tsx
+++ app/1/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import CrossIcon from "@/public/icons/icon-cross-small.svg";
+import PrevIcon from "@/public/icons/arrow-prev.svg";
 
 const BookingCard = () => {
   return (
@@ -51,12 +52,18 @@ const Booking = () => {
         </div>
         <div className="flex flex-col items-center gap-9 px-10 pb-12 pt-7">
           <div className="flex gap-6 text-gray-950">
-            <button className="text-xs">Prev</button>
-            <p className="flex flex-col items-center gap-1">
-              <span className="text-lg font-bold ">Sep. 24</span>
-              <span className="text-xs font-bold">Today</span>
+            <button className="text-xs">
+              <PrevIcon className="text-xl" />
+            </button>
+            <p className="relative flex flex-col items-center gap-1">
+              <span className="text-lg font-bold ">24 Sep</span>
+              {/* <span className="absolute -bottom-6 left-0 right-0 m-auto rounded-full bg-green-600 px-2 py-1 text-center text-xs font-medium text-white">
+                Today
+              </span> */}
             </p>
-            <button className="text-xs">Next</button>
+            <button className="text-xs">
+              <PrevIcon className="rotate-180 text-xl" />
+            </button>
           </div>
           <div className="flex w-full flex-col items-center gap-3">
             <BookingCard />
